f17ca92c70fdf04a6c3c45352439b4f3fbbe6e1a
Kyle Thayer <kthayer@cs.washington.edu>
2020-02-12 12:12:31 -0800

    bla bla

--- analysis.R
+++ analysis.R
@@ -3,3 +3,6 @@ library(dplyr)
 
 most_college <- filter(midwest, percollege == max(percollege)) %>%
   pull(county)
+
+example_plot <- ggplot(data = midwest) +
+  geom_hex(mapping = aes(x = percollege, y = percadultpoverty))
